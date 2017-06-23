8685daa98ac77bafcbd5fa9f5ce7ee763094f54a
Diego Rodriguez <diego.rodriguez@cern.ch>
2017-06-23 14:06:55 +0200

    admin: remove create user profile option
    * Uses just `_displayname` for listing users since `username` property
      is retrieving `_displayname` behind the scenes. `_displayname` has
      been chosen because it allows sorting.
    
    * Restricts the edition to `username` and `full_name` since editing
      `user_id` is error prone and `_displayname` shouldn't be changed
      (it is handled internally).
    
    Signed-off-by: Diego Rodriguez <diego.rodriguez@cern.ch>

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of Invenio.
-# Copyright (C) 2016 CERN.
+# Copyright (C) 2016, 2017 CERN.
 #
 # Invenio is free software; you can redistribute it
 # and/or modify it under the terms of the GNU General Public License as
@@ -57,5 +57,5 @@ def test_admin(app):
                 follow_redirects=True
             )
             assert res.status_code == 200
-            assert b'Display Name' in (res.get_data())
+            assert b'Username' in (res.get_data())
             assert b'Full Name' in (res.get_data())
