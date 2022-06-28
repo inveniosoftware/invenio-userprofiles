229d7be0bc5c5ff53e1c6a673747f7bc60a7c002
Christoph Ladurner <christoph.ladurner@tugraz.at>
2022-06-28 22:17:27 +0200

    fix sphinx warning of not found reference target

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,8 @@ extensions = [
     "sphinx.ext.viewcode",
 ]
 
+nitpick_ignore = [("py:attr", "meta")]
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
